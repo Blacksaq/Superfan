ebe549270090199b904235a644e7a7bcc10b522e
Blacksaq <ahtls24@gmail.com>
2017-12-12 01:32:37 -0800

    Update wf.js

--- warframes/wf.js
+++ warframes/wf.js
@@ -1,3 +1,15 @@
-function warDrum(e){
+function warDrum(e){	
+	document.getElementById("excal").pause();
+	document.getElementById("loki").pause();
 	document.getElementById("mesa").play();
+}
+function whatYouAre(e){
+	document.getElementById("loki").pause();
+	document.getElementById("mesa").pause();
+	document.getElementById("excal").play();
+}
+function shadowStalker(e){
+	document.getElementById("mesa").pause();
+	document.getElementById("excal").pause();
+	document.getElementById("loki").play();
 }
